3d5d0743c220ebeca29992f6a5a8ba811e2b9f90
bitbyteme <$bitbyteme@gmail.com>
2011-11-08 15:07:14 -0800

    Darwin bits-MacBook-Pro.local 11.2.0 at 2011.11.08 15:07:14 -0800

--- installer.ub.server.bash
+++ installer.ub.server.bash
@@ -1,11 +1,25 @@
 #! /bin/sh
 
-#
-##
-#
+sudo echo '\c'
+# adding apt-get restricted repositories by editing /etc/apt/sources.list file
+if ! [ -f '/etc/apt/sources.list.bak' ]; then  
+   sudo chmod 777 /etc/apt
+   sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
+   sudo cat /etc/apt/sources.list.bak | sed -e 's;^# deb http; deb http;' -e 's;^# deb-src ; deb-src ;' > /etc/apt/sources.list
+   sudo chmod 755 /etc/apt
+fi
 
+sudo apt-get -y update
+sudo apt-get -y upgrade
+pkgsBasic='build-essential curl wget git-core openssl libssl-dev openssh-server openssh-client'
+pkgsExtra='vim-nox zsh ' 
 
-echo "hi, I'm ub.server"
+sudo apt-get -y install $pkgsBasic $pkgsExtra 
+sudo apt-get -y autoremove
+
+echo 'press enter to reboot, or ^C to quit \c'; read
+reboot
+
 
 
 
