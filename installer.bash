680764afdd7a32e6ac41e1a8697cc50795f21fc5
bitbyteme <$bitbyteme@gmail.com>
2011-11-08 16:45:37 -0800

    Darwin bits-MacBook-Pro.local 11.2.0 at 2011.11.08 16:45:37 -0800

--- installer.bash
+++ installer.bash
@@ -6,10 +6,9 @@
 
 plat="$(uname)"
 GIT_PATH='https://raw.github.com/bitbyteme/aurum/master/'
-url_linux="$GIT_PATH/installer.ub.server.bash"
-url_mac="$GIT_PATH/installer.lion.bash"
 
 if [ "$plat" = 'Linux' ]; then
+   url_linux="$GIT_PATH/installer.ub.server.bash"
    ub_config="$GIT_PATH/installer.ub.config.bash"
    
    python -c "import urllib2; print urllib2.urlopen('$url_linux').read()"|sh
@@ -17,6 +16,7 @@ if [ "$plat" = 'Linux' ]; then
    
 
 elif [ "$plat" = 'Darwin' ]; then
+   url_mac="$GIT_PATH/installer.lion.bash"
    python -c "import urllib2; print urllib2.urlopen('$url_mac').read()" |sh
 fi
 
